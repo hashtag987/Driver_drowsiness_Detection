fd1bcb9e4e4f5fa685195dbc9f9f5017e226e22b
hashtag987 <sri33arun@gmail.com>
2022-04-23 07:08:43 +0530

    3

---
+++ app.py
@@ -0,0 +1,19 @@
+import torch
+from matplotlib import pyplot as plt
+import numpy as np
+import cv2
+
+model = torch.hub.load('ultralytics/yolov5', 'custom', path='yolov5/runs/train/exp17/weights/last.pt', force_reload=True)
+cap = cv2.VideoCapture(0)
+while cap.isOpened():
+    ret, frame = cap.read()
+    
+    # Make detections 
+    results = model(frame)
+    
+    cv2.imshow('YOLO', np.squeeze(results.render()))
+    
+    if cv2.waitKey(10) & 0xFF == ord('q'):
+        break
+cap.release()
+cv2.destroyAllWindows()
